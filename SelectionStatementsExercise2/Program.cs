0d25511cb42def7d1d3179208232e2d614b4edef
Zack Moyal <zack.moyal@gmail.com>
2023-12-31 06:45:46 -0800

    Exercise 2 - switch statements

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is quite challenging, but some like myself are very good at it.");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is very interesting and will open your mind up.");
+                    break;
+                case "English":
+                    Console.WriteLine("Great choice. I struggled a lot with English up until college.");
+                    break;
+                case "History":
+                    Console.WriteLine("Awesome. Those who do not remember the past are condemned to repeat it.");
+                    break;
+                case "Physical Education":
+                    Console.WriteLine("Solid. Intelligence and skill can only function at the peak of their capacity when the body is healthy and strong.");
+                    break;
+                default:
+                    Console.WriteLine($"Wow, I haven't taken that subject before. {subject} sounds fun!");
+                    break;
+            }
         }
     }
 }
